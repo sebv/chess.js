87245b6612b6c72bdddb79133f6eb242534fad25
sebv <seb.vincent@gmail.com>
2013-06-13 08:44:07 +0800

    Constant initialisation cleanup.

--- chess.js
+++ chess.js
@@ -28,6 +28,10 @@
  *----------------------------------------------------------------------------*/
 
 (function() {
+
+  /*****************************************************************************
+   * Pieces representation
+   ****************************************************************************/
     
   var BLACK = 'b';
   var WHITE = 'w';
@@ -43,15 +47,55 @@
 
   var SYMBOLS = 'pnbrqkPNBRQK';
 
-  var DEFAULT_POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+  /*****************************************************************************
+   * 0x88 board representation
+   ****************************************************************************/
 
-  var POSSIBLE_RESULTS = ['1-0', '0-1', '1/2-1/2', '*'];
+   /*
+      SQUARES should contain the following:
 
+      a8:   0, b8:   1, c8:   2, d8:   3, e8:   4, f8:   5, g8:   6, h8:   7,
+      a7:  16, b7:  17, c7:  18, d7:  19, e7:  20, f7:  21, g7:  22, h7:  23,
+      a6:  32, b6:  33, c6:  34, d6:  35, e6:  36, f6:  37, g6:  38, h6:  39,
+      a5:  48, b5:  49, c5:  50, d5:  51, e5:  52, f5:  53, g5:  54, h5:  55,
+      a4:  64, b4:  65, c4:  66, d4:  67, e4:  68, f4:  69, g4:  70, h4:  71,
+      a3:  80, b3:  81, c3:  82, d3:  83, e3:  84, f3:  85, g3:  86, h3:  87,
+      a2:  96, b2:  97, c2:  98, d2:  99, e2: 100, f2: 101, g2: 102, h2: 103,
+      a1: 112, b1: 113, c1: 114, d1: 115, e1: 116, f1: 117, g1: 118, h1: 119
+
+      todo: use standard version where a1 is 0
+   */
+  var SQUARES_KEYS = [],
+      SQUARES = {};
+
+  for(var  i=8; i>0; i--)
+    for(var j=0; j<8; j++) 
+      SQUARES_KEYS.push(String.fromCharCode('a'.charCodeAt()+j) + i);
+
+  for(var i=0; i< SQUARES_KEYS.length; i++) 
+    SQUARES[SQUARES_KEYS[i]] = i + 8*Math.floor(i / 8);
+
+  // todo: should not be needed 
+  var RANK_1 = 7;
+  var RANK_2 = 6;
+  var RANK_3 = 5;
+  var RANK_4 = 4;
+  var RANK_5 = 3;
+  var RANK_6 = 2;
+  var RANK_7 = 1;
+  var RANK_8 = 0;
+
+  /*****************************************************************************
+   * Pieces movement
+   ****************************************************************************/
+
+  // using 0x88 representation
   var PAWN_OFFSETS = {
     b: [16, 32, 17, 15],
     w: [-16, -32, -17, -15]
   };
 
+  // using 0x88 representation
   var PIECE_OFFSETS = {
     n: [-18, -33, -31, -14,  18, 33, 31,  14],
     b: [-17, -15,  17,  15],
@@ -60,6 +104,14 @@
     k: [-17, -16, -15,   1,  17, 16, 15,  -1]
   };
 
+
+  /*****************************************************************************
+   * Attack related constants
+   ****************************************************************************/
+
+  var SHIFTS = { p: 0, n: 1, b: 2, r: 3, q: 4, k: 5 };
+
+  // using SHIFTS masks
   var ATTACKS = [
     20, 0, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0, 0,20, 0,
     0 ,20, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0,20, 0, 0,
@@ -78,6 +130,7 @@
     20, 0, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0, 0,20
   ];
 
+  // using 0x88 representation
   var RAYS = [
     17 ,  0,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0,  0, 15, 0,
     0  , 17,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0, 15,  0, 0,
@@ -96,7 +149,9 @@
     -15,  0,  0,  0,  0,  0,  0,-16,  0,  0,  0,  0,  0,  0,-17
   ];
 
-  var SHIFTS = { p: 0, n: 1, b: 2, r: 3, q: 4, k: 5 };
+  /*****************************************************************************
+   * Move related constants
+   ****************************************************************************/
 
   var FLAGS = {
     NORMAL: 'n',
@@ -118,48 +173,20 @@
     QSIDE_CASTLE: 64
   };
 
-  var RANK_1 = 7;
-  var RANK_2 = 6;
-  var RANK_3 = 5;
-  var RANK_4 = 4;
-  var RANK_5 = 3;
-  var RANK_6 = 2;
-  var RANK_7 = 1;
-  var RANK_8 = 0;
-
-  /*****************************************************************************
-   * Initialising SQUARES with 0x88 representation.
-   ****************************************************************************/
-
-   /*
-      SQUARES should contain the following:
-      SQUARES = {
-      a8:   0, b8:   1, c8:   2, d8:   3, e8:   4, f8:   5, g8:   6, h8:   7,
-      a7:  16, b7:  17, c7:  18, d7:  19, e7:  20, f7:  21, g7:  22, h7:  23,
-      a6:  32, b6:  33, c6:  34, d6:  35, e6:  36, f6:  37, g6:  38, h6:  39,
-      a5:  48, b5:  49, c5:  50, d5:  51, e5:  52, f5:  53, g5:  54, h5:  55,
-      a4:  64, b4:  65, c4:  66, d4:  67, e4:  68, f4:  69, g4:  70, h4:  71,
-      a3:  80, b3:  81, c3:  82, d3:  83, e3:  84, f3:  85, g3:  86, h3:  87,
-      a2:  96, b2:  97, c2:  98, d2:  99, e2: 100, f2: 101, g2: 102, h2: 103,
-      a1: 112, b1: 113, c1: 114, d1: 115, e1: 116, f1: 117, g1: 118, h1: 119
-      };
-   */
-  var SQUARES_KEYS = [],
-      SQUARES = {};
-
-  for(var  i=8; i>0; i--)
-    for(var j=0; j<8; j++) 
-      SQUARES_KEYS.push(String.fromCharCode('a'.charCodeAt()+j) + i);
-
-  for(var i=0; i< SQUARES_KEYS.length; i++) 
-    SQUARES[SQUARES_KEYS[i]] = i + 8*Math.floor(i / 8);
-
   var ROOKS = {
     w: [{square: SQUARES.a1, flag: BITS.QSIDE_CASTLE},
         {square: SQUARES.h1, flag: BITS.KSIDE_CASTLE}],
     b: [{square: SQUARES.a8, flag: BITS.QSIDE_CASTLE},
         {square: SQUARES.h8, flag: BITS.KSIDE_CASTLE}]
   };
+
+  /*****************************************************************************
+   * Other constants
+   ****************************************************************************/
+
+  var DEFAULT_POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+  var POSSIBLE_RESULTS = ['1-0', '0-1', '1/2-1/2', '*'];
 
   /*****************************************************************************
    * UTILITY FUNCTIONS
